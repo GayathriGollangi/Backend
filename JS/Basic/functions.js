c8fa7857b6b733ed961ef32a6b5d18316df0dba7
gayathrigollangi <gayathrigollangii@gmail.com>
2024-12-16 20:51:03 +0530

    functions

---
+++ JS/Basic/functions.js
@@ -0,0 +1,18 @@
+// arrow function
+
+const Greeting = ()=>{
+    console.log("Hello, World!");
+}
+Greeting(); // Output: Hello, World!
+
+// Anonymous function
+const Welcome = function(){
+    console.log("Welcome to JS");
+}
+Welcome();
+
+//function
+function Display(){
+    console.log("Hello");
+}
+Display();
